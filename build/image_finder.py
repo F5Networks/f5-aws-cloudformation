99727250c2560dc340c45d5f23924a9eae4c6b33
User1 Desktop <smooth.alg@gmail.com>
2016-07-09 16:49:41 -0700

    Updated to include hourly utility templates

--- build/image_finder.py
+++ build/image_finder.py
@@ -171,13 +171,13 @@ def main():
     image_finder_obj = BigIpImageFinder()
 
     # Hourly Region Map ( Need to wait for v12/v13 to be released before can use Cloudinit )
-    HourlyRegionMap = image_finder_obj.getBigipRegionMap(license="hourly", version="11.6.0.6.204.442", throughput="1gbps", regions=regions)
+    HourlyRegionMap = image_finder_obj.getBigipRegionMap(license="hourly", version="12.1.0.1.1.1447", throughput="1gbps", regions=regions)
     
     with open('cached-hourly-region-map.json', 'w') as outfile:
         json.dump(HourlyRegionMap, outfile, sort_keys = True, indent = 2, ensure_ascii=False)
 
     # BYOL Region Map:
-    ByolRegionMap = image_finder_obj.getBigipRegionMap(license="byol", version="12.0.0.1.14.628", throughput="1gbps", regions=regions)
+    ByolRegionMap = image_finder_obj.getBigipRegionMap(license="byol", version="12.1.0.1.1.1447", throughput="1gbps", regions=regions)
 
     with open('cached-byol-region-map.json', 'w') as outfile:
         json.dump(ByolRegionMap, outfile, sort_keys = True, indent = 2, ensure_ascii=False)

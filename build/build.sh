b2dbad5f3d2078b2990b0e5a43a059160a0b1633
crosbygw <g.crosby@f5.com>
2016-11-04 13:00:29 -0700

    folder changes

--- build/build.sh
+++ build/build.sh
@@ -11,66 +11,66 @@ POOL_DIR="/pool"
 # python image_finder.py
 
 # Network Only
-python master_template.py -s network -a 1 -n 1 > ../experimental/reference/network-only/network-only-1AZ-for-1nic-bigip.template
-python master_template.py -s network -a 1 -n 2 > ../experimental/reference/network-only/network-only-1AZ-for-2nic-bigip.template
-python master_template.py -s network -a 1 -n 3 > ../experimental/reference/network-only/network-only-1AZ-for-3nic-bigip.template
+python master_template.py -s network -a 1 -n 1 > ../experimental/reference/1nic/network-only/f5-network-only-1AZ-for-1nic-bigip.template
+python master_template.py -s network -a 1 -n 2 > ../experimental/reference/2nic/network-only/f5-network-only-1AZ-for-2nic-bigip.template
+python master_template.py -s network -a 1 -n 3 > ../experimental/reference/3nic/network-only/f5-network-only-1AZ-for-3nic-bigip.template
 
-python master_template.py -s network -a 2 -n 1 > ../experimental/reference/network-only/network-only-2AZ-for-1nic-bigip.template
-python master_template.py -s network -a 2 -n 2 > ../experimental/reference/network-only/network-only-2AZ-for-2nic-bigip.template
-python master_template.py -s network -a 2 -n 3 > ../experimental/reference/network-only/network-only-2AZ-for-3nic-bigip.template
+python master_template.py -s network -a 2 -n 1 > ../experimental/reference/1nic/network-only/f5-network-only-2AZ-for-1nic-bigip.template
+python master_template.py -s network -a 2 -n 2 > ../experimental/reference/2nic/network-only/f5-network-only-2AZ-for-2nic-bigip.template
+python master_template.py -s network -a 2 -n 3 > ../experimental/reference/3nic/network-only/f5-network-only-2AZ-for-3nic-bigip.template
 
 # Security Groups Only
-python master_template.py -s security_groups -n 1 > ../experimental/reference/security-group-creation/security-groups-for-1nic-bigip.template
-python master_template.py -s security_groups -n 2 > ../experimental/reference/security-group-creation/security-groups-for-2nic-bigip.template
-python master_template.py -s security_groups -n 3 > ../experimental/reference/security-group-creation/security-groups-for-3nic-bigip.template
+python master_template.py -s security_groups -n 1 > ../experimental/reference/1nic/security-group-creation/f5-security-groups-for-1nic-bigip.template
+python master_template.py -s security_groups -n 2 > ../experimental/reference/2nic/security-group-creation/f5-security-groups-for-2nic-bigip.template
+python master_template.py -s security_groups -n 3 > ../experimental/reference/3nic/security-group-creation/f5-security-groups-for-3nic-bigip.template
 
 
 # Infra (Everything But BIG-IPs)
-python master_template.py -s infra -a 1 -n 1 > ../experimental/reference/infrastructure-only/infra-only-1AZ-for-1nic-bigip.template
-python master_template.py -s infra -a 1 -n 2 > ../experimental/reference/infrastructure-only/infra-only-1AZ-for-2nic-bigip.template
-python master_template.py -s infra -a 1 -n 3 > ../experimental/reference/infrastructure-only/infra-only-1AZ-for-3nic-bigip.template
+python master_template.py -s infra -a 1 -n 1 > ../experimental/reference/1nic/infrastructure-only/f5-infra-only-1AZ-for-1nic-bigip.template
+python master_template.py -s infra -a 1 -n 2 > ../experimental/reference/2nic/infrastructure-only/f5-infra-only-1AZ-for-2nic-bigip.template
+python master_template.py -s infra -a 1 -n 3 > ../experimental/reference/3nic/infrastructure-only/f5-infra-only-1AZ-for-3nic-bigip.template
 
-python master_template.py -s infra -a 2 -n 1 > ../experimental/reference/infrastructure-only/infra-only-2AZ-for-1nic-bigip.template
-python master_template.py -s infra -a 2 -n 2 > ../experimental/reference/infrastructure-only/infra-only-2AZ-for-2nic-bigip.template
-python master_template.py -s infra -a 2 -n 3 > ../experimental/reference/infrastructure-only/infra-only-2AZ-for-3nic-bigip.template
+python master_template.py -s infra -a 2 -n 1 > ../experimental/reference/1nic/infrastructure-only/f5-infra-only-2AZ-for-1nic-bigip.template
+python master_template.py -s infra -a 2 -n 2 > ../experimental/reference/2nic/infrastructure-only/f5-infra-only-2AZ-for-2nic-bigip.template
+python master_template.py -s infra -a 2 -n 3 > ../experimental/reference/3nic/infrastructure-only/f5-infra-only-2AZ-for-3nic-bigip.template
 
 
 
 # BIGIP Stacks
-python master_template.py -s full -n 1 -l hourly > ../experimental/learning-stacks/full-stack-hourly-1nic-bigip.template
-python master_template.py -s full -n 2 -l hourly > ../experimental/learning-stacks/full-stack-hourly-2nic-bigip.template
-python master_template.py -s full -n 3 -l hourly > ../experimental/learning-stacks/full-stack-hourly-3nic-bigip.template
+python master_template.py -s full -n 1 -l hourly > ../experimental/learning-stacks/standalone/1nic/f5-full-stack-hourly-1nic-bigip.template
+python master_template.py -s full -n 2 -l hourly > ../experimental/learning-stacks/standalone/2nic/f5-full-stack-hourly-2nic-bigip.template
+python master_template.py -s full -n 3 -l hourly > ../experimental/learning-stacks/standalone/3nic/f5-full-stack-hourly-3nic-bigip.template
 
-python master_template.py -s existing -n 1 -l hourly > ../experimental/standalone/1nic/existing-stack-hourly-1nic-bigip.template
-python master_template.py -s existing -n 2 -l hourly > ../experimental/standalone/2nic/existing-stack-hourly-2nic-bigip.template
-python master_template.py -s existing -n 3 -l hourly > ../experimental/standalone/3nic/existing-stack-hourly-3nic-bigip.template
+python master_template.py -s existing -n 1 -l hourly > ../experimental/standalone/1nic/f5-existing-stack-hourly-1nic-bigip.template
+python master_template.py -s existing -n 2 -l hourly > ../experimental/standalone/2nic/f5-existing-stack-hourly-2nic-bigip.template
+python master_template.py -s existing -n 3 -l hourly > ../experimental/standalone/3nic/f5-existing-stack-hourly-3nic-bigip.template
 
-python master_template.py -s full -n 1 -l byol > ../experimental/learning-stacks/full-stack-byol-1nic-bigip.template
-python master_template.py -s full -n 2 -l byol > ../experimental/learning-stacks/full-stack-byol-2nic-bigip.template
-python master_template.py -s full -n 3 -l byol > ../experimental/learning-stacks/full-stack-byol-3nic-bigip.template
+python master_template.py -s full -n 1 -l byol > ../experimental/learning-stacks/standalone/1nic/f5-full-stack-byol-1nic-bigip.template
+python master_template.py -s full -n 2 -l byol > ../experimental/learning-stacks/standalone/2nic/f5-full-stack-byol-2nic-bigip.template
+python master_template.py -s full -n 3 -l byol > ../experimental/learning-stacks/standalone/3nic/f5-full-stack-byol-3nic-bigip.template
 
-python master_template.py -s existing -n 1 -l byol > ../experimental/standalone/1nic/existing-stack-byol-1nic-bigip.template
-python master_template.py -s existing -n 2 -l byol > ../experimental/standalone/2nic/existing-stack-byol-2nic-bigip.template
-python master_template.py -s existing -n 3 -l byol > ../experimental/standalone/3nic/existing-stack-byol-3nic-bigip.template
+python master_template.py -s existing -n 1 -l byol > ../experimental/standalone/1nic/f5-existing-stack-byol-1nic-bigip.template
+python master_template.py -s existing -n 2 -l byol > ../experimental/standalone/2nic/f5-existing-stack-byol-2nic-bigip.template
+python master_template.py -s existing -n 3 -l byol > ../experimental/standalone/3nic/f5-existing-stack-byol-3nic-bigip.template
 
-python master_template.py -s full -n 1 -l bigiq > ../experimental/learning-stacks/full-stack-bigiq-license-pool-1nic-bigip.template
-python master_template.py -s full -n 2 -l bigiq > ../experimental/learning-stacks/full-stack-bigiq-license-pool-2nic-bigip.template
-python master_template.py -s full -n 3 -l bigiq > ../experimental/learning-stacks/full-stack-bigiq-license-pool-3nic-bigip.template
+python master_template.py -s full -n 1 -l bigiq > ../experimental/learning-stacks/reference/1nic/bigiq/f5-full-stack-bigiq-license-pool-1nic-bigip.template
+python master_template.py -s full -n 2 -l bigiq > ../experimental/learning-stacks/reference/2nic/bigiq/f5-full-stack-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s full -n 3 -l bigiq > ../experimental/learning-stacks/reference/3nic/bigiq/f5-full-stack-bigiq-license-pool-3nic-bigip.template
 
-python master_template.py -s existing -n 1 -l bigiq > ../experimental/solutions/bigiq/existing-stack-bigiq-license-pool-1nic-bigip.template
-python master_template.py -s existing -n 2 -l bigiq > ../experimental/solutions/bigiq/existing-stack-bigiq-license-pool-2nic-bigip.template
-python master_template.py -s existing -n 3 -l bigiq > ../experimental/solutions/bigiq/existing-stack-bigiq-license-pool-3nic-bigip.template
+python master_template.py -s existing -n 1 -l bigiq > ../experimental/reference/1nic/bigiq/f5-existing-stack-bigiq-license-pool-1nic-bigip.template
+python master_template.py -s existing -n 2 -l bigiq > ../experimental/reference/2nic/bigiq/f5-existing-stack-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s existing -n 3 -l bigiq > ../experimental/reference/3nic/bigiq/f5-existing-stack-bigiq-license-pool-3nic-bigip.template
 
 
 # Components 
 
 ## WAF
-python master_template.py -s full -n 1 -l hourly -c waf > ../experimental/learning-stacks/full-stack-hourly-1nic-bigip-w-waf.template
-python master_template.py -s existing -n 1 -l hourly -c waf > ../experimental/standalone/1nic/existing-stack-hourly-1nic-bigip-w-waf.template
-python master_template.py -s full -n 1 -l byol -c waf > ../experimental/learning-stacks/full-stack-byol-1nic-bigip-w-waf.template
-python master_template.py -s existing -n 1 -l byol -c waf > ../experimental/standalone/1nic//existing-stack-byol-1nic-bigip-w-waf.template
-python master_template.py -s full -n 1 -l bigiq -c waf > ../experimental/learning-stacks/full-stack-bigiq-license-pool-1nic-bigip-w-waf.template
-python master_template.py -s existing -n 1 -l bigiq -c waf > ../experimental/solutions/bigiq/existing-stack-bigiq-license-pool-1nic-bigip-w-waf.template
+python master_template.py -s full -n 1 -l hourly -c waf > ../experimental/learning-stacks/reference/1nic/waf/f5-full-stack-hourly-1nic-bigip-w-waf.template
+python master_template.py -s existing -n 1 -l hourly -c waf > ../experimental/reference/1nic/waf/f5-existing-stack-hourly-1nic-bigip-w-waf.template
+python master_template.py -s full -n 1 -l byol -c waf > ../experimental/learning-stacks/reference/1nic/waf/f5-full-stack-byol-1nic-bigip-w-waf.template
+python master_template.py -s existing -n 1 -l byol -c waf > ../experimental/reference/1nic/waf/f5-existing-stack-byol-1nic-bigip-w-waf.template
+python master_template.py -s full -n 1 -l bigiq -c waf > ../experimental/learning-stacks/reference/1nic/bigiq/f5-full-stack-bigiq-license-pool-1nic-bigip-w-waf.template
+python master_template.py -s existing -n 1 -l bigiq -c waf > ../experimental/reference/1nic/bigiq/f5-existing-stack-bigiq-license-pool-1nic-bigip-w-waf.template
 
 
 ## Clusters 
@@ -78,47 +78,47 @@ python master_template.py -s existing -n 1 -l bigiq -c waf > ../experimental/sol
 ### SAME AZ
 
 #### HOURLY
-python master_template.py -s full -n 2 -l hourly -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-hourly-2nic-bigip.template
-python master_template.py -s existing -n 2 -l hourly -H same-az > ../experimental/cluster/2nic/existing-stack-same-az-cluster-hourly-2nic-bigip.template
+python master_template.py -s full -n 2 -l hourly -H same-az > ../experimental/learning-stacks/cluster/2nic/f5-full-stack-same-az-cluster-hourly-2nic-bigip.template
+python master_template.py -s existing -n 2 -l hourly -H same-az > ../experimental/cluster/2nic/f5-existing-stack-same-az-cluster-hourly-2nic-bigip.template
 #### WITH WAF 
-python master_template.py -s full -n 2 -l hourly -c waf -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-hourly-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l hourly -c waf -H same-az > ../experimental/cluster/2nic/existing-stack-same-az-cluster-hourly-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l hourly -c waf -H same-az > ../experimental/learning-stacks/reference/2nic/waf/f5-full-stack-same-az-cluster-hourly-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l hourly -c waf -H same-az > ../experimental/reference/2nic/waf/f5-existing-stack-same-az-cluster-hourly-2nic-bigip-w-waf.template
 
 #### BYOL
-python master_template.py -s full -n 2 -l byol -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-byol-2nic-bigip.template
-python master_template.py -s existing -n 2 -l byol -H same-az > ../experimental/cluster/2nic/existing-stack-same-az-cluster-byol-2nic-bigip.template
+python master_template.py -s full -n 2 -l byol -H same-az > ../experimental/learning-stacks/cluster/2nic/f5-full-stack-same-az-cluster-byol-2nic-bigip.template
+python master_template.py -s existing -n 2 -l byol -H same-az > ../experimental/cluster/2nic/f5-existing-stack-same-az-cluster-byol-2nic-bigip.template
 #### WITH WAF 
-python master_template.py -s full -n 2 -l byol -c waf -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-byol-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l byol -c waf -H same-az > ../experimental/cluster/2nic/existing-stack-same-az-cluster-byol-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l byol -c waf -H same-az > ../experimental/learning-stacks/reference/2nic/waf/f5-full-stack-same-az-cluster-byol-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l byol -c waf -H same-az > ../experimental/reference/2nic/waf/f5-existing-stack-same-az-cluster-byol-2nic-bigip-w-waf.template
 
 #### BIQ-IQ
-python master_template.py -s full -n 2 -l bigiq -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-bigiq-license-pool-2nic-bigip.template
-python master_template.py -s existing -n 2 -l bigiq -H same-az > ../experimental/solutions/bigiq/existing-stack-same-az-cluster-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s full -n 2 -l bigiq -H same-az > ../experimental/learning-stacks/reference/2nic/bigiq/f5-full-stack-same-az-cluster-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s existing -n 2 -l bigiq -H same-az > ../experimental/reference/2nic/bigiq/f5-existing-stack-same-az-cluster-bigiq-license-pool-2nic-bigip.template
 #### WITH WAF
-python master_template.py -s full -n 2 -l bigiq -c waf -H same-az > ../experimental/learning-stacks/full-stack-same-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l bigiq -c waf -H same-az > ../experimental/solutions/bigiq/existing-stack-same-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l bigiq -c waf -H same-az > ../experimental/learning-stacks/reference/2nic/bigiq/f5-full-stack-same-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l bigiq -c waf -H same-az > ../experimental/reference/2nic/bigiq/f5-existing-stack-same-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
 
 
 
 ### ACROSS AZ
 
 #### HOURLY
-python master_template.py -s full -n 2 -l hourly -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-hourly-2nic-bigip.template
-python master_template.py -s existing -n 2 -l hourly -H across-az > ../experimental/cluster/2nic/existing-stack-across-az-cluster-hourly-2nic-bigip.template
+python master_template.py -s full -n 2 -l hourly -H across-az > ../experimental/learning-stacks/cluster/2nic/f5-full-stack-across-az-cluster-hourly-2nic-bigip.template
+python master_template.py -s existing -n 2 -l hourly -H across-az > ../experimental/cluster/2nic/f5-existing-stack-across-az-cluster-hourly-2nic-bigip.template
 #### WITH WAF 
-python master_template.py -s full -n 2 -l hourly -c waf -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-hourly-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l hourly -c waf -H across-az > ../experimental/cluster/2nic/existing-stack-across-az-cluster-hourly-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l hourly -c waf -H across-az > ../experimental/learning-stacks/reference/2nic/waf/f5-full-stack-across-az-cluster-hourly-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l hourly -c waf -H across-az > ../experimental/reference/2nic/waf/f5-existing-stack-across-az-cluster-hourly-2nic-bigip-w-waf.template
 
 #### BYOL
-python master_template.py -s full -n 2 -l byol -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-byol-2nic-bigip.template
-python master_template.py -s existing -n 2 -l byol -H across-az > ../experimental/cluster/2nic/existing-stack-across-az-cluster-byol-2nic-bigip.template
+python master_template.py -s full -n 2 -l byol -H across-az > ../experimental/learning-stacks/cluster/2nic/f5-full-stack-across-az-cluster-byol-2nic-bigip.template
+python master_template.py -s existing -n 2 -l byol -H across-az > ../experimental/cluster/2nic/f5-existing-stack-across-az-cluster-byol-2nic-bigip.template
 #### WITH WAF 
-python master_template.py -s full -n 2 -l byol -c waf -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-byol-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l byol -c waf -H across-az > ../experimental/cluster/2nic/existing-stack-across-az-cluster-byol-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l byol -c waf -H across-az > ../experimental/learning-stacks/reference/2nic/waf/f5-full-stack-across-az-cluster-byol-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l byol -c waf -H across-az > ../experimental/reference/2nic/waf/f5-existing-stack-across-az-cluster-byol-2nic-bigip-w-waf.template
 
 #### BIQ-IQ
-python master_template.py -s full -n 2 -l bigiq -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-bigiq-license-pool-2nic-bigip.template
-python master_template.py -s existing -n 2 -l bigiq -H across-az > ../experimental/solutions/bigiq/existing-stack-across-az-cluster-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s full -n 2 -l bigiq -H across-az > ../experimental/learning-stacks/reference/2nic/bigiq/f5-full-stack-across-az-cluster-bigiq-license-pool-2nic-bigip.template
+python master_template.py -s existing -n 2 -l bigiq -H across-az > ../experimental/reference/2nic/bigiq/f5-existing-stack-across-az-cluster-bigiq-license-pool-2nic-bigip.template
 #### WITH WAF
-python master_template.py -s full -n 2 -l bigiq -c waf -H across-az > ../experimental/learning-stacks/full-stack-across-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
-python master_template.py -s existing -n 2 -l bigiq -c waf -H across-az > ../experimental/solutions/bigiq/existing-stack-across-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
+python master_template.py -s full -n 2 -l bigiq -c waf -H across-az > ../experimental/learning-stacks/reference/2nic/bigiq/f5-full-stack-across-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
+python master_template.py -s existing -n 2 -l bigiq -c waf -H across-az > ../experimental/reference/2nic/bigiq/f5-existing-stack-across-az-cluster-bigiq-license-pool-2nic-bigip-w-waf.template
---
+++ experimental/solutions/autoscale/README.md
@@ -0,0 +1,127 @@
+# Autoscaling the BIG-IP Web Application Firewall in AWS
+[![Slack Status](https://f5cloudsolutions.herokuapp.com/badge.svg)](https://f5cloudsolutions.herokuapp.com)
+[![Doc Status](http://readthedocs.org/projects/f5-sdk/badge/?version=latest)](https://f5.com/solutions/deployment-guides)
+
+## Introduction
+This project implements autoscaling of BIG-IP Web Application Firewall (WAF) systems in Amazon Web Services using the AWS CloudFormation template **autoscale-bigip.template**. As traffic increases or decreases, the number of BIG-IP instances automatically increases or decreases accordingly.
+
+## Documentation
+See the project documentation on (TBD).
+
+## BIG-IP deployment and configuration
+
+All BIG-IPs are deployed with a single interface attached to a public subnet. Advanced traffic management functionality is provided through use of BIG-IP Local Traffic Manager (LTM) and Application Security Manager (ASM). The default **Best 25Mbs** image available in the AWS marketplace is used to license these modules.
+All of the BIG-IP configuration is performed at device bootup using `CloudInit`. This can be seen in autoscale-bigip.template CloudFormation template. In general, CloudInit is used to :
+
+- Set the BIG-IP hostname, NTP, and DNS settings
+- Configure an IAM (Identity and Access Management) role with policies allowing the BIG-IP to make authenticated calls to AWS HTTPS endpoints.
+- Create an HTTP virtual server with a Web Application Firewall policy
+- Deploy integration with EC2 Autoscale and CloudWatch services for scaling of the BIG-IP tier.
+
+
+### Installation ###
+Download the CloudFormation template from https://github.com/f5networks and use it to create a stack in AWS Cloudformation either using the AWS Console or AWS CLI
+
+**AWS Console**
+
+ From the AWS Console main page: 
+   1. Under AWS Services, click **CloudFormation**.
+   2. Click the **Create Stack** button 
+   3. In the Choose a template area, click **Upload a template to Amazon S3**.
+   4. Click **Choose File** and then browse to the **autoscale-bigip.template** file.
+ 
+ 
+ **AWS CLI**
+ 
+ From the AWS CLI, use the following command syntax:
+ ```
+ aws cloudformation create-stack --stack-name my-autoscale-bigip --template-body file:///fullfilepath/autoscale-bigip.template --parameters file:///fullfilepath/autoscale-bigip-parameters.json --capabilities CAPABILITY_NAMED_IAM
+```
+
+### Usage ###
+Use this template to automate the autoscale implementation by providing the parameter values. You can use or change the default parameter values, which are defined in the AWS CloudFormation template on the AWS Console.  If using the AWS CLI, use the following JSON format parameter file
+
+
+| Parameter | Required | Description |
+| --- | --- | --- |
+| deploymentName | x | Deployment Name - Used in creating objects |
+| vpc | x | Common VPC for whole deployment |
+| availabilityZones | x | Availability zones in which BIG-IP is being deployed |
+| subnets | x | AZ Public or External Subnet IDs |
+| bigipSecurityGroup | x | Pre-existing security group for BIG-IP |
+| bigipElasticLoadBalancer | x | Elastic Load Balancer group for all BIG-IPs. "Default": "BigipElasticLoadBalancer" |
+| keyName | x | Name of an existing EC2 KeyPair to enable SSH access to the instance |
+| sshLocation | x | The IP address range that can be used to SSH to the EC2 instances |
+| instanceType | x | F5 BIG-IP Instance Type. "Default": "m4.2xlarge" |
+| performanceType | x | F5 BIG-IP Performance Type. "Default": "Best" |
+| throughput | x | F5 BIG-IP Throughput. "Default": "25-Mbps" |
+| adminPassword | x | Please enter your BIG-IP Admin Password |
+| managementGuiPort | x | Port to use for the managment GUI. "Default": 443 |
+| timezone | x | Enter a Olson timezone string from /usr/share/zoneinfo. "Default": "UTC" |
+| ntpServers | x | Enter a space list of NTP servers. ex. 0.pool.ntp.org 1.pool.ntp.org. "Default": "0.pool.ntp.org" |
+| scalingMinSize | x | Enter the minimum number of BIG-IP instances (1-8) to be available in the AutoScale Group. "Default": "1" |
+| scalingMaxSize | x | Enter the maximum number of BIG-IP instances (2-8) that can be created in the AutoScale Group. "Default": "3" |
+| scaleDownBytesThreshold | x | Enter bytes to begin Scaling Down. "Default": "10000" |
+| scaleUpBytesThreshold | x | Enter bytes to begin Scaling Up. "Default": "35000" |
+| notificationEmail |  | Enter a valid email address to send AutoScaling Event Notifications |
+| virtualServicePort | x | The port for the Virtual Service on the Big-IP |
+| applicationPort | x | The Pool Member Port. "Default": "80" |
+| appInternalElbDnsName | x | DNS of the ELB used for the application. "Default": "XXXXXXX.region.elb.amazonaws.com" |
+
+
+
+
+
+Example **autoscale-bigip-parameters.json**
+```
+[
+	{
+		"ParameterKey":"vpc",
+		"ParameterValue":"vpc-1ffef478"
+	},
+	{
+		"ParameterKey":"availabilityZones",
+		"ParameterValue":"us-east-1a,us-east-1b,us-east-1c"
+	},
+	{
+		"ParameterKey":"subnets",
+		"ParameterValue":"subnet-88d30fa5,subnet-2dc44b64,subnet-413cec1a"
+	},
+	{
+		"ParameterKey":"bigipSecurityGroup",
+		"ParameterValue":"sg-13ee026e"
+	},
+	{
+		"ParameterKey":bigipElasticLoadBalancer,
+		"ParameterValue":"A1-BigipElb"
+	},
+	{
+		"ParameterKey":"keyName",
+		"ParameterValue":"awskeypair-east"
+	},
+	{
+		"ParameterKey":"keyName",
+		"ParameterValue":"awskeypair-east"
+	},
+	{
+		"ParameterKey":"keyName",
+		"ParameterValue":"awskeypair-east"
+	},
+	{
+		"ParameterKey":"keyName",
+		"ParameterValue":"awskeypair-east"
+	},
+	{
+		"ParameterKey":"adminPassword",
+		"ParameterValue":"GoF5!"
+	},
+		"ParameterKey":"notificationEmail",
+		"ParameterValue":"k.koh@f5.com"
+	},
+
+	{
+		ParameterKey:"appInternalElbDnsName",
+		ParameterValue:"internal-A1-AppElb-911355308.us-east-1.elb.amazonaws.com"
+	}
+]
+```

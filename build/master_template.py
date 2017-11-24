551f9e94f7ddd2040a7c1ef978c90a875564b1a9
crosbygw <g.crosby@f5.com>
2017-11-24 00:00:04 -0800

    View release notes for v2.7.1

--- build/master_template.py
+++ build/master_template.py
@@ -132,14 +132,14 @@ def main():
     # Build variables used for QA
 
     ### Template Version
-    version = "2.6.1"
+    version = "2.7.1"
     ### Big-IP mapped
-    BIGIP_VERSION = "13.0.0.2.0.1671"
+    BIGIP_VERSION = "13.0.0.3.0.1679"
     ### Cloudlib Branch
     branch_cloud = "v3.4.1"
     branch_aws = "v1.5.0"
     branch_cloud_iapps = "v1.1.1"
-    ### Build verifyHash file from published verifyHash on github
+    ### Build verifyHash file from published verifyHash github
     urls = [ 'https://raw.githubusercontent.com/F5Networks/f5-cloud-libs/' + str(branch_cloud) + '/dist/verifyHash' ]
     for url in urls:
         try:

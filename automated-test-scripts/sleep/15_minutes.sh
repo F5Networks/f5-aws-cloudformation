4bc277379102cd7a0ccb1430bcf942add235fdff
kashcheev <a.kashcheev@f5.com>
2021-08-19 20:58:21 +0000

    Release commited to v5.13.0 tag

--- automated-test-scripts/sleep/15_minutes.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 16
-#  replayEnabled = false
-#  replayTimeout = 0
-
-sleep 900
-echo "PASS"
--- automated-test-scripts/sleep/35_minutes.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 36
-#  replayEnabled = false
-#  replayTimeout = 0
-
-sleep 2100
-echo "PASS"
